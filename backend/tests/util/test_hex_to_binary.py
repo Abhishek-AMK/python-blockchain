7609fae72b385dd7223b9c5d9b3bd262da2016cd
Abhishek <amkb222@gmail.com>
2024-06-10 23:54:50 +0530

    new commit

---
+++ backend/tests/util/test_hex_to_binary.py
@@ -0,0 +1,11 @@
+from backend.util.hex_to_binary import hex_to_binary
+
+def test_hex_to_binary():
+    original_number = 789
+    hex_number = hex(original_number)[2:]
+    binary_number = hex_to_binary(hex_number)
+    
+    assert int(binary_number, 2) == original_number
+    
+    # hex_to_binary_crypto_hash = hex_to_binary('test-data')
+    # assert hex_to_binary_crypto_hash == hex_to_binary_crypto_hash
