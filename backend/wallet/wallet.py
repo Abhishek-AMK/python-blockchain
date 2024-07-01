d9b3bcfc838eda212f34fecfeb580316c9b27c60
Abhishek <amkb222@gmail.com>
2024-07-02 00:18:33 +0530

    new commit

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -3,9 +3,11 @@ import uuid
 
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature, encode_dss_signature
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.exceptions import InvalidSignature
+
 
 class Wallet:
     """
@@ -19,26 +21,41 @@ class Wallet:
             default_backend()
             )
         self.public_key = self.private_key.public_key()
+        self.serialize_public_key()
         
     def sign(self, data):
         """
         Generate a signature based on the data using the local private key.
         """
-        data_as_json = json.dumps(data)  # Convert data to JSON string
-        data_as_bytes = data_as_json.encode('utf-8')  # Encode JSON string to bytes
-        return self.private_key.sign(
-            data_as_bytes,
+        return decode_dss_signature(self.private_key.sign(
+            json.dumps(data).encode('utf-8'),
             ec.ECDSA(hashes.SHA256())
-        )
+        ))
         
+    def serialize_public_key(self):
+        """
+        Reset the public key to its serialized version.
+        """
+        self.public_key = self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ).decode('utf-8')
+    
     @staticmethod
     def verify(public_key, data, signature):
         """
         Verify a signature based on the original public key and data.
         """
+        deserialized_public_key = serialization.load_pem_public_key(
+            public_key.encode('utf-8'),
+            default_backend()
+        )
+
+        (r, s) = signature
+        
         try:
-            public_key.verify(
-                signature,
+            deserialized_public_key.verify(
+                encode_dss_signature(r, s),
                 json.dumps(data).encode('utf-8'),
                 ec.ECDSA(hashes.SHA256())
             )
