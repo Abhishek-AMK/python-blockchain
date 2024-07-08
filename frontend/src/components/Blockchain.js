ea384ff5d80312a7d104517f2c41db7f0a3019ba
Abhishek <amkb222@gmail.com>
2024-07-08 23:53:04 +0530

    new commit

---
+++ frontend/src/components/Blockchain.js
@@ -0,0 +1,27 @@
+import React,{useState,useEffect} from 'react';
+import { API_BASE_URL } from '../config';
+
+function Blockchain() {
+    const [blockchain, setBlockchain] = useState([]);
+
+    useEffect(() => {
+        fetch(`${API_BASE_URL}/blockchain`)
+            .then(response => response.json())
+            .then(json => setBlockchain(json));
+    }, []);
+
+    return (
+        <div className="Blockchain">
+            <h3>Blockchain</h3>
+            <div>
+                {
+                    blockchain.map(block => (
+                        <div key={block.hash}>{JSON.stringify(block)}</div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Blockchain;
